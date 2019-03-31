59bd16c82034e4ca933e7c0271d2ead254b7ec6f
Khurram <mkhurram900@gmail.com>
2019-04-01 02:26:15 +0500

    fixed

---
+++ src/Http/Middleware/Authentication.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace MSACommon\MSACommon\Http\Middleware;
+
+use App\Services\AuthService;
+use Closure;
+use Auth;
+use MSACommon\MSACommon\Common\ApiResponseCodesBook;
+use MSACommon\MSACommon\Exceptions\APIException;
+use MSACommon\MSACommon\Services\AuthUserService;
+use View;
+
+class Authentication
+{
+    /* @var $authUserService AuthUserService*/
+    private $authUserService;
+
+    public function __construct(AuthUserService $authUserService){
+        $this->authUserService = $authUserService;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!session()->has('token')) {
+            throw new APIException(ApiResponseCodesBook::NOT_LOGGED_IN);
+        }
+
+        $user = $this->authUserService->getUser();
+
+        return $next($request);
+    }
+}
