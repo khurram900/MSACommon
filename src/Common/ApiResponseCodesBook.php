0848e78ff01e662e9124547c88858fba8206516f
Khurram <mkhurram900@gmail.com>
2019-03-31 19:41:41 +0500

    helpers added

---
+++ src/Common/ApiResponseCodesBook.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MSACommon\MSACommon\Common;
+
+class ApiResponseCodesBook
+{
+
+    const SUCCESS = [
+        'outcomeCode' => '0',
+        'outCome' => 'SUCCESS'
+    ];
+
+    const FORM_VALIDATION_ERROR = [
+        'outcomeCode' => '1',
+        'outCome' => 'FORM_VALIDATION_ERROR'
+    ];
+
+}
--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,25 @@
 <?php
 
-function testasdsad(){
-    dd("asdasdasd");
+function msacommon_successResponse($results = []){
+    return response()->json((new \MSACommon\MSACommon\Common\ApiResponse())->setResults(
+        $results
+    )->toArray());
+}
+
+
+function msacommon_errorResponse(){
+    return (new \MSACommon\MSACommon\Common\ApiResponse())->setResults(
+        $results
+    );
+}
+
+
+function msacommon_makeErrorArray(array &$errorsArray,$key,$message){
+    $errorsArray[$key] = $message;
+}
+
+function msacommon_convertErrorBagToCompaitableArray(array &$preparedErrors,array $errorsArray){
+    foreach ($errorsArray as $key=>$errors){
+        msacommon_makeErrorArray($preparedErrors,$key,$errors[0]);
+    }
 }
